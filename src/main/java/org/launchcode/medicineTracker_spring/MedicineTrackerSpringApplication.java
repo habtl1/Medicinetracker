3ffa366a12c18ea2218b54563fa336bede1efc39
habtl1 <lhabtemichael1@gamil.com>
2024-11-05 21:45:41 -0600

    setup spring boot application

---
+++ src/main/java/org/launchcode/medicineTracker_spring/MedicineTrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.medicineTracker_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineTrackerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineTrackerSpringApplication.class, args);
+	}
+
+}
